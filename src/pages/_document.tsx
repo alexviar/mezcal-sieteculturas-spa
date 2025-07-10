aa2e9072f14707eb21df8b61f6c7e7374f74d91b
Alexander <alexander.va@hotmail.com>
2025-07-10 16:24:12 -0400

    feat: add background image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,15 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="es">
       <Head />
-      <body>
+      <body style={{
+        backgroundImage: 'url(/background.png)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}>
         <Main />
         <NextScript />
       </body>

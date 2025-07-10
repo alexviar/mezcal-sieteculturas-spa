a31e269d8861cd35b603092a7057289b486b1f4a
Alexander <alexander.va@hotmail.com>
2025-07-10 17:04:08 -0400

    feat: update shop home

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,7 +1,10 @@
 import { getErrorMessage } from "@/api/getErrorMessage";
 import { useGetCatalogQuery } from "@/api/services/storeApi";
+import Image from "next/image";
 import { useState } from "react";
-import { Alert, Col, Row } from "react-bootstrap";
+import { Alert, Button, Col, Ratio, Row } from "react-bootstrap";
+import { FaChevronRight } from "react-icons/fa";
+import Logo from "../assets/logo.png";
 import Loader from "./loader";
 import { PaginationComponent } from "./pagination";
 import { ProductCard } from "./ProductCard";
@@ -18,8 +21,25 @@ export default function Products() {
   const products = productsPage?.data
 
   function renderContent() {
+    if (getProducts.data?.total == 0) {
+      return (
+        <div id="products" className="w-100 px-3 py-4 bg-body">
+          <div className="w-100 p-5 text-center d-flex flex-column align-items-center">
+            <Image
+              src={Logo}
+              alt="No products available"
+              width={150}
+              height={150}
+              className="mb-4"
+            />
+            <p className="fs-5 fw-bold">¡Ups! Parece que no hay productos aquí todavía.</p>
+            <p className="text-muted">Estamos trabajando para traer las mejores selecciones para ti. ¡Vuelve pronto!</p>
+          </div>
+        </div>
+      )
+    }
     if (products) {
-      return <div className="w-100 px-3 py-4">
+      return <div id="products" className="w-100 px-3 py-4 bg-body">
         <Row className="g-4 justify-content-center">
           {products.map((product) => {
             return (
@@ -42,6 +62,34 @@ export default function Products() {
 
   return (
     <>
+      <div
+        className="w-100 p-5 d-flex align-items-center justify-content-center flex-column"
+        style={{
+          maxWidth: '28rem',
+          height: '100dvh',
+          marginTop: -64
+        }}
+      >
+        <Ratio aspectRatio="1x1">
+          <div>
+            <Image
+              src={Logo}
+              alt="Loading"
+              fill
+              priority
+              className="object-fit-contain"
+              style={{ maxWidth: '28rem' }}
+            />
+          </div>
+        </Ratio>
+        <p className="text-center mb-0">
+          Descubre la tradición ancestral del mezcal, donde cada gota cuenta una historia de siete culturas milenarias
+        </p>
+        <Button as="a" href="#products" className="mt-4 py-3 px-4 d-flex align-items-center gap-3 rounded-pill">
+          Explora nuestros productos
+          <FaChevronRight />
+        </Button>
+      </div>
       {renderContent()}
       {productsPage && <PaginationComponent
         pagination={{

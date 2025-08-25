01379e85bcb42cca7ff46410f589fca6bb3bba5e
Alexander <alexander.va@hotmail.com>
2025-08-25 00:27:16 -0600

    feat: add back button to layout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,19 +3,23 @@ import { appName } from "@/configs/app";
 import { RootState } from "@/models/redux/store";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { ReactNode } from "react";
+import { LuChevronLeft } from "react-icons/lu";
 import { useSelector } from "react-redux";
 import Logo from "../assets/logo.png";
 
 type HeaderProps = {
+  showBackButton?: boolean
+  backTo?: string
   hideBrandLogo?: boolean
   title?: ReactNode
   subtitle?: ReactNode
 }
 
-export default function Header({ hideBrandLogo, title, subtitle }: HeaderProps) {
-  const pathname = usePathname()
+export default function Header({ hideBrandLogo, showBackButton = false, backTo, title, subtitle }: HeaderProps) {
+  const router = useRouter();
+  const pathname = usePathname();
   const user = useSelector(
     (state: RootState) => state.user.user
   );
@@ -23,19 +27,35 @@ export default function Header({ hideBrandLogo, title, subtitle }: HeaderProps) 
   return (
     <nav className="header">
       <div className="header-container">
-        <Link href="/" className="brand-item">
-          {!hideBrandLogo && <Image
-            src={Logo}
-            width={60}
-            height={60}
-            alt={`${appName} logo`}
-            priority
-          />}
-          {title && <div>
-            <h1>{title}</h1>
-            {subtitle && <p>{subtitle}</p>}
-          </div>}
-        </Link>
+        <div className="d-flex align-items-center gap-2">
+          {showBackButton && (
+            <button
+              onClick={() => backTo ? router.replace(backTo) : router.back()}
+              className="btn btn-link p-0"
+              aria-label="Volver atrás"
+            >
+              <LuChevronLeft size={24} color="white" />
+            </button>
+          )}
+          <Link href="/" className="brand-item">
+            {!hideBrandLogo && (
+              <Image
+                src={Logo}
+                width={40}
+                height={40}
+                alt={`${appName} logo`}
+                className="me-2"
+                priority
+              />
+            )}
+            {title && (
+              <div>
+                <h1 className="mb-0">{title}</h1>
+                {subtitle && <p className="mb-0 small">{subtitle}</p>}
+              </div>
+            )}
+          </Link>
+        </div>
         <div className="action-container">
           {pathname !== '/login' && <Link href={user ? "/dashboard/home" : "/login"} className="action-container-item login">
             <User />

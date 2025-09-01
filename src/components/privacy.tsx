12a2e49d3318905b0d4957fde541235622320b6b
Alexander <alexander.va@hotmail.com>
2025-09-01 14:06:22 -0600

    feat: add privacy policy and terms & conditions pages

--- src/components/privacy.tsx
+++ src/components/privacy.tsx
@@ -1,8 +1,51 @@
+import { appName } from "@/configs/app";
+
 /* eslint-disable react/no-unescaped-entities */
 export default function Privacy() {
   return (
-    <div className="privacy-container">
-      <h1>AVISO DE PRIVACIDAD</h1>
+    <div className="privacy-container p-3">
+      <h1 className="text-accent">AVISO DE PRIVACIDAD INTEGRAL</h1>
+      <p>{appName} es el responsable del uso y protección de sus datos personales, y al respecto le informamos lo siguiente:</p>
+      <h2 className="text-accent">¿Para qué fines utilizaremos sus datos personales?</h2>
+      <p>Los datos personales que recabamos de usted, los utilizaremos para las siguientes finalidades que son necesarias para el servicio que solicita:</p>
+      <ul>
+        <li>Respuesta a mensajes del formulario de contacto</li>
+        <li>Prestación de cualquier servicio solicitado</li>
+        <li>Facturación y cobro</li>
+        <li>Envío y entrega de productos</li>
+      </ul>
+      <h2 className="text-accent">¿Qué datos personales utilizaremos para estos fines?</h2>
+      <p>Para llevar a cabo las finalidades descritas en el presente aviso de privacidad, utilizaremos los siguientes datos personales:</p>
+      <ul>
+        <li>Datos de identificación y contacto</li>
+        <li>Datos laborales</li>
+        <li>Datos académicos</li>
+        <li>Datos facturación</li>
+      </ul>
+      <h2 className="text-accent">¿Con quién compartimos su información personal y para qué fines?</h2>
+      <p>Le informamos que sus datos personales son compartidos fuera del país con las siguientes personas, empresas, organizaciones o autoridades distintas a nosotros, para los siguientes fines:</p>
+      <ul>
+        <li>Fuera del país</li>
+        <li>Personas externas a la empresa.</li>
+      </ul>
+      <h2 className="text-accent">¿Cómo puede acceder, rectificar o cancelar sus datos personales, u oponerse a su uso o ejercer la revocación de consentimiento?</h2>
+      <p>Usted tiene derecho a conocer qué datos personales tenemos de usted, para qué los utilizamos y las condiciones del uso que les damos (Acceso). Asimismo, es su derecho solicitar la corrección de su información personal en caso de que esté desactualizada, sea inexacta o incompleta (Rectificación); que la eliminemos de nuestros registros o bases de datos cuando considere que la misma no está siendo utilizada adecuadamente (Cancelación); así como oponerse al uso de sus datos personales para fines específicos (Oposición). Estos derechos se conocen como derechos ARCO.</p>
+      <p>Para el ejercicio de cualquiera de los derechos ARCO, debe enviar una petición vía correo electrónico y deberá contener:</p>
+      <ul>
+        <li>Nombre completo del titular.</li>
+        <li>Domicilio.</li>
+        <li>Teléfono.</li>
+        <li>Correo electrónico usado en esta aplicación.</li>
+        <li>Copia de una identificación oficial adjunta.</li>
+        <li>Asunto «Derechos ARCO»</li>
+      </ul>
+      <p>Descripción el objeto del escrito, los cuales pueden ser de manera enunciativa más no limitativa los siguientes: Revocación del consentimiento para tratar sus datos personales; y/o Notificación del uso indebido del tratamiento de sus datos personales; y/o Ejercitar sus Derechos ARCO, con una descripción clara y precisa de los datos a Acceder, Rectificar, Cancelar o bien, Oponerse. En caso de Rectificación de datos personales, deberá indicar la modificación exacta y anexar la documentación soporte; es importante en caso de revocación del consentimiento, que tenga en cuenta que no en todos los casos podremos atender su solicitud o concluir el uso de forma inmediata, ya que es posible que por alguna obligación legal requiramos seguir tratando sus datos personales. Asimismo, usted deberá considerar que para ciertos fines, la revocación de su consentimiento implicará que no le podamos seguir prestando el servicio que nos solicitó, o la conclusión de su relación con nosotros.</p>
+      <h2 className="text-accent">¿En cuántos días le daremos respuesta a su solicitud?</h2>
+      <p>10 días</p>
+      <h2 className="text-accent">¿Por qué medio le comunicaremos la respuesta a su solicitud?</h2>
+      <p>Al mismo correo electrónico de donde se envió la petición.</p>
+      <h2 className="text-accent">El uso de tecnologías de rastreo en nuestra aplicación</h2>
+      <p>Le informamos que en nuestra aplicación utilizamos cookies, web beacons u otras tecnologías, a través de las cuales es posible monitorear su comportamiento como usuario, así como brindarle un mejor servicio y experiencia al navegar en nuestra aplicación. Los datos personales que obtenemos de estas tecnologías de rastreo son los siguientes:</p>
     </div>
   );
 }
--- src/components/terms.tsx
+++ src/components/terms.tsx
@@ -1,10 +1,36 @@
 /* eslint-disable react/no-unescaped-entities */
 
+import { appName } from "@/configs/app";
+
 
 export default function Terms() {
   return (
-    <div className="terms-container">
-      <h1>TÉRMINOS Y CONDICIONES</h1>
+    <div className="terms-container p-3">
+      <h1 className="text-accent">TÉRMINOS Y CONDICIONES</h1>
+      <p>Todo uso de esta aplicación está condicionado a tu aceptación de estos términos y condiciones y del aviso de privacidad desplegado en esta página.</p>
+      <p>Nos reservamos el derecho de modificar, a nuestra exclusiva discreción, los términos y condiciones, así como el aviso de privacidad aquí anexos en cualquier momento. Es tu responsabilidad informarte de los términos, condiciones y aviso de privacidad vigentes, los cuales estarán siempre disponibles en esta página. Las modificaciones a los términos y condiciones y al aviso de privacidad entrarán en vigor en el momento en que sean agregados a esta aplicación.</p>
+      <p>El material en esta aplicación está protegido por las leyes de propiedad intelectual y otras leyes aplicables; por lo que queda prohibido copiar, reproducir, distribuir, publicar de nuevo, cargar, postear, compartir o transmitir esta aplicación de manera alguna sin el consentimiento previo y por escrito del titular de los derechos de propiedad intelectual relacionados a la misma aplicación.</p>
+      <p>{appName} es el titular de todas las marcas que aparecen en esta aplicación y los derechos de propiedad intelectual sobre las mismas pertenecen de forma exclusiva a cada uno de sus titulares.</p>
+      <p>El material en este sitio se ofrece exclusivamente para fines legítimos y comerciales.</p>
+      <p>{appName} no será responsable por daños, perjuicios o lesiones relacionados con el uso, acceso o incapacidad de uso del material en esta aplicación. Los materiales en esta aplicación se proveen “tal cual”, sin garantías de ningún tipo, ya sean explícitas o implícitas.</p>
+      <p>Podremos restringir, suspender o terminar tu acceso a esta aplicación en cualquier momento si consideramos que has violado cualquiera de estos términos y condiciones. Dicha restricción, suspensión o terminación será sin perjuicio a los derechos que pudiéramos ejercer en relación con la violación de estos términos y condiciones.</p>
+      <p>Respetamos los derechos de propiedad intelectual de los demás y exigimos que las personas que usan esta aplicación hagan lo mismo. Si consideras que alguna obra tuya ha sido copiada en una manera tal que constituya una violación a tus derechos de propiedad intelectual, por favor envía la siguiente información al contacto que se indica más adelante:</p>
+      <ul>
+        <li>Una descripción de las obras protegidas cuyos derechos de autor, en tu opinión, han sido violados.</li>
+        <li>Una descripción de la supuesta actividad infractora y dónde está ubicado el supuesto material infractor.</li>
+        <li>Una declaración en la que manifiestes de buena fe que el uso en cuestión no está autorizado por ti, el titular de los derechos de propiedad intelectual, sus representantes o que contraviene alguna disposición legal.</li>
+        <li>Una firma electrónica o física de la persona autorizada para actuar en nombre del titular de los derechos de propiedad intelectual.</li>
+        <li>Una declaración, bajo protesta de decir verdad, de que la información contenida en tu informe es correcta y que eres el titular o usuario autorizado de los derechos de propiedad intelectual presuntamente infringidos.</li>
+      </ul>
+      <h2 className="text-accent">USO DE LA INFORMACIÓN</h2>
+      <p>{appName} se obliga a proteger la información que recopilamos en esta aplicación, de acuerdo a lo establecido en el Aviso de Privacidad exhibido en esta página.</p>
+      <p>{appName} podrá exigir tu registro para participar en las dinámicas que se realicen en la presente aplicación y usar la información que nos proporciones al registrarte para la entrega de premios; dicho uso de tu información siempre estará condicionado al Aviso de Privacidad.</p>
+      <p>Generamos correos electrónicos de respuesta a los correos que recibimos de tu parte, así como lo de los otros usuarios.</p>
+      <p>Esta aplicación puede contener vínculos, sugerencias o referencia a otros sitios que están fuera de nuestro control. Te invitamos a leer los términos y condiciones de dichos sitios antes de utilizarlos, ya que {appName} no es responsable por el contenido de los mismos.</p>
+      <p>Al usar esta aplicación, expresas tu consentimiento a los términos y condiciones y al Aviso de Privacidad de esta aplicación. Si no estás de acuerdo con alguno de los términos y condiciones o con el Aviso de Privacidad te sugerimos no utilices esta aplicación.</p>
+      <p>Si tienes preguntas o inquietudes relacionadas con estos términos y condiciones, por favor contáctanos a través de los medios disponibles en la aplicación.</p>
+      <p>Podremos modificar estos términos y condiciones en cualquier momento. Recomendamos que revises esta página con regularidad para asegurarte de que has leído la versión más reciente.</p>
+      <p>Las leyes y los reglamentos que regulan la presente aplicación son las de México. {appName} © 2025.</p>
     </div>
   );
 }
